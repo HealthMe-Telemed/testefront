2d55b3de77d1e9e072c68b2476276f8e3d46ea40
Vini1604 <vinicius.oliveira1998@hotmail.com>
2024-05-02 21:38:46 -0300

    Correcao css novo agendamento

--- src/components/Novo_Agendamento.vue
+++ src/components/Novo_Agendamento.vue
@@ -333,7 +333,7 @@ export default {
 
 section {
   min-height: 100vh;
-  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background: linear-gradient(-45deg, #3d93b7, #70b8d6);
   background-size: 400% 400%;
   animation: gradient 10s ease infinite;
 }
